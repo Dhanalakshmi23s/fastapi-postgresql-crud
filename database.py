361575470cacae1e58adebdd6cab5db30e93152b
Dhanalakshmi s <131767292+Dhanalakshmi23s@users.noreply.github.com>
2025-06-07 15:15:47 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL='postgresql://postgres:Admin#123@localhost/Database'
+SQLALCHEMY_DATABASE_URL='postgresql://username:passsword@localhost/Database'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
